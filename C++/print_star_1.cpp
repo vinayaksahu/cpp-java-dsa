f7e269f09085a978196be9e9d4569f984f227e34
vinayaksahu <vinayaksahu3@gmail.com>
2022-11-23 20:00:16 +0530

    cpp/printstar

--- C++/print_star_1.cpp
+++ C++/print_star_1.cpp
@@ -1,25 +1,28 @@
-#include<iostream>
+// Print *
+// ****
+// ****
+// ****
+// ****
+
+#include <iostream>
 using namespace std;
 
-int main()
-{
-    
-    int n;
-    cin >> n;
+int main() {
 
-    int i = 0;
-    while (i < n)
+    int n = 4;
+
+    int i = 1;
+    while (i <= n)
     {
-        int j = 0;
-        while (j < n)
+        int j = 1;
+        while (j <= n)
         {
-            cout << "*  ";
-            j = j + 1;
+            cout <<"*";
+            j++;
         }
         cout <<endl;
-        i = i + 1;
+        i++;
     }
     
 
-    return 0;
 }
