0ac60b8d5fff59ddee09539cb7e15d54ad3fdd09
vinayaksahu <vinayaksahu3@gmail.com>
2022-11-25 14:56:51 +0530

    byte num1 = 127;

--- Java/Introduction/DataTypes/Byte.java
+++ Java/Introduction/DataTypes/Byte.java
@@ -1,10 +1,8 @@
 class Byte {
     public static void main(String[] args) {
-        
-        byte num1, num2;
-
-        num1 = 127;
-        //num2 = 129;
+       
+        byte num1 = 127;
+        //byte num2 = 129;
 
         System.out.println(num1);  //127
         //System.out.println(num2); //error: out of range
