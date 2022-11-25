cd7641c53a1babaded870d698b027e02072b8a5e
vinayaksahu <vinayaksahu3@gmail.com>
2022-11-25 23:02:57 +0530

    whileloop done

---
+++ Java/Flow Control/WhileLoop/While.java
@@ -0,0 +1,11 @@
+public class While {
+    public static void main(String[] args) {
+        
+        //declare while loop
+        int i = 1;
+        while (i <= 5) {
+            System.out.println(i);
+            i++;
+        }
+    }    
+}
