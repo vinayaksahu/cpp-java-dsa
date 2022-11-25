29397439c2d31655d1e863b2ea9d03e9bcab6273
vinayaksahu <vinayaksahu3@gmail.com>
2022-11-25 15:01:28 +0530

    short range -32768 to 32767

---
+++ Java/Introduction/DataTypes/Short.java
@@ -0,0 +1,12 @@
+import javax.swing.plaf.synth.SynthDesktopIconUI;
+
+public class Short {
+    public static void main(String[] args) {
+        
+        //short range -32768 to 32767
+
+        short number = -32768;
+
+        System.out.println(number); //print -32768
+    }
+}
