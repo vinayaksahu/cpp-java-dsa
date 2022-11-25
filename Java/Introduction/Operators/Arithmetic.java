576360482d0903c27f8f37d2e05f3626f0063acb
vinayaksahu <vinayaksahu3@gmail.com>
2022-11-25 18:34:15 +0530

    Arithmetic operator done

---
+++ Java/Introduction/Operators/Arithmetic.java
@@ -0,0 +1,24 @@
+public class Arithmetic {
+
+    public static void main(String[] args) {
+        
+        //Declare variables
+        int a = 1, b = 2;
+
+        //Addition operator
+        System.out.println("a + b = " + (a + b));
+
+        //subtraction operator
+        System.out.println("a - b = " + (a - b));
+
+        //multiplication operator
+        System.out.println("a * b = " + (a * b));
+
+        //division operator
+        System.out.println("a / b = " + (a / b));
+
+        //modulo operator
+        System.out.println("a % b = " + (a % b));
+
+    }
+}
