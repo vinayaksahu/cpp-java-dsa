7e9ee648aa634fdc7a51a735286ca1762e50cbe0
vinayaksahu <vinayaksahu3@gmail.com>
2022-11-27 02:41:19 +0530

    Prime in java error removed

--- Java/Problems/Prime.java
+++ Java/Problems/Prime.java
@@ -1,28 +1,22 @@
-import javax.sql.rowset.spi.SyncResolver;
-
 public class Prime {
-    
-    public static void main(String[] args) {
-
-        int n = 47;
-        boolean isprime = true;
-    
-        int i = 2;
-        while (i <= n/2) 
-            if (n % i == 0)
-            {
-                isprime = false;
-                break;
-            }
-            
-            i++;
-        }
-        if (is_prime)
-        {
-            System.out.println("Prime"); 
-        }
-        else
-        { System.out.println("Not Prime"); }
-            
+   public static void main(String[] args)
+   {
+      int n = 47;
+      boolean isprime = true;
+      int i = 2;
+      while(i <= n/2)
+      {
+         if(n % i == 0)
+         {
+            isprime = false;
+            break;
+         }
+         i++;
+      }
+      
+      if(isprime)
+         System.out.println("Prime Number");
+      else
+         System.out.println("Not a Prime Number.");
+   }
 }
-
