bc1bd5ce19ff1552d594144edc773831193156fe
vinayaksahu <vinayaksahu3@gmail.com>
2022-11-26 23:33:51 +0530

    swap without using temp

--- swap2num.cpp
+++ swap2num.cpp
@@ -10,10 +10,16 @@ int main()
 
 
     //swap numbers using temp
-    temp = n1;
+  /*  temp = n1;
     n1 = n2;
-    n2 = temp;
+    n2 = temp;*/
 
+    //swap without using temp
+    n2 = n1 + n2;   //total = n1 + n2
+    n1 = n2 - n1;   //n1(n2 value) = total - n1
+    n2 = n2 - n1;   //n2 (n1 value)= total - n1
+
+    
     cout <<"n1: "<<n1 <<endl<<"n2: "<<n2<<endl;
     
     return 0;
