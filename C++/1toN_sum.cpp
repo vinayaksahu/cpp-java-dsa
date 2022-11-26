efaa901307768a79ec4147dbf297634e51dc36f4
vinayaksahu <vinayaksahu3@gmail.com>
2022-11-27 00:07:40 +0530

    sum of natural number using For loop in C++

--- C++/1toN_sum.cpp
+++ C++/1toN_sum.cpp
@@ -6,15 +6,23 @@ int main(void)
     int n=5;
     //cin >> n;
     int sum = 0;
-    int i = 1;
+
+/*    int i = 1;
     while (i <= n)
     {
 
         sum = sum + i;
 
         i = i + 1;
+    }*/
+
+    //for loop
+    for (int i = 1; i <=n; i++)
+    {
+        sum += i;
     }
     
+
     cout << sum <<endl;
 
     return 0;
