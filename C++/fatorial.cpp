b31acde87c391e622adfd46f8859f83bd468a04c
vinayaksahu <vinayaksahu3@gmail.com>
2022-11-27 00:55:00 +0530

    factorial of given number by user in cpp

---
+++ C++/fatorial.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+    
+int main() {
+        
+        int num, fact = 1;
+
+        cin >> num;
+
+        for (int i = 1; i <= num; i++) {
+            fact *= i;
+        }
+        cout <<fact;
+
+}
