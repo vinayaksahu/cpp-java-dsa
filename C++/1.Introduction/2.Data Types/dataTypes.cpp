7b7e1ab0f31da3aa24e7f4cb4d2f1e2decee6406
vinayaksahu <vinayaksahu3@gmail.com>
2022-11-28 23:31:07 +0530

    data types done

---
+++ C++/1.Introduction/2.Data Types/dataTypes.cpp
@@ -0,0 +1,29 @@
+void main()
+{
+    /*
+    C++ Fundamental Data Types
+    The table below shows the fundamental data types, their meaning, and their sizes (in bytes):
+
+    Data Type	Meaning	Size (in Bytes)
+    int	Integer	2 or 4
+    float	Floating-point	4
+    double	Double Floating-point	8
+    char	Character	1
+    wchar_t	Wide Character	2
+    bool	Boolean	1
+    void	Empty	0
+    */
+
+   int salary = 85000;
+
+   float area = 64.74;
+   double volume = 134.64534;
+   double distance = 45E12;    // 45E12 is equal to 45*10^12
+
+   char ch = 'h';
+
+   wchar_t test = L'ם';  // storing Hebrew character;
+
+   bool cond = false;
+
+}
