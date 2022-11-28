b8e45145a814924fd001ab987b8903fb2b8769d9
vinayaksahu <vinayaksahu3@gmail.com>
2022-11-29 00:07:04 +0530

    operators topic covered

---
+++ C++/1.Introduction/5.Operators/bitwise.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+    int a = 4;
+    int b = 6;
+
+    cout<<" a&b :" << (a&b) << endl;
+    cout<<" a|b :" << (a|b) << endl;
+    cout<<" ~a :" << ~a << endl;
+    cout<<" a%b :" << (a%b) << endl;
+
+    cout<<(a>>1) <<endl;
+    cout<<(b>>2) <<endl;
+    cout<<(a<<1) <<endl;
+    cout<<(a<<2) <<endl;
+
+    
+}
