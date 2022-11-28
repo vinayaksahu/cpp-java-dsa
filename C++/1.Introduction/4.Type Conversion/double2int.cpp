3f200335113582e02ee789b9d4384c83f94904eb
vinayaksahu <vinayaksahu3@gmail.com>
2022-11-28 23:52:44 +0530

    type conversion topic done

---
+++ C++/1.Introduction/4.Type Conversion/double2int.cpp
@@ -0,0 +1,25 @@
+//Working of Implicit type-conversion
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+   int num_int;
+   double num_double = 9.99;
+
+   // implicit conversion
+   // assigning a double value to an int variable
+   num_int = num_double;
+
+   cout << "num_int = " << num_int << endl;
+   cout << "num_double = " << num_double << endl;
+
+   return 0;
+}
+
+/*
+    Output:
+    num_int = 9
+    num_double = 9.99
+*/
