57f8fafd1848941989c6adcdd732ee214bbc19d9
vinayaksahu <vinayaksahu3@gmail.com>
2022-12-02 23:18:03 +0530

    total number of occuraence of the key

---
+++ C++/Problems/Leetcode/13.2.TotalOccurence.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+
+int firstOcc (int arr[], int n, int key) {
+
+    int s = 0;
+    int e = n - 1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+
+    while (s <= e) {
+        if (arr[mid] == key) {
+            ans = mid;
+            e = mid - 1;
+        }
+        else if (key > arr[mid]) {
+            s = mid + 1;
+        }
+        else if (key < arr[mid]) {
+            e = mid - 1;
+        }
+        mid = s + (e-s)/2;        
+    }
+    return ans;
+}
+
+int lastOcc (int arr[], int n, int key) {
+
+    int s = 0;
+    int e = n - 1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+
+    while (s <= e) {
+        if (arr[mid] == key) {
+            ans = mid;
+            s = mid + 1;
+        }
+        else if (key > arr[mid]) {
+            s = mid + 1;
+        }
+        else if (key < arr[mid]) {
+            e = mid - 1;
+        }
+        mid = s + (e-s)/2;        
+    }
+    return ans;
+}
+
+
+int main() {
+
+    int arr[] = {0, 0, 0, 1, 1, 1, 1, 1, 1};
+
+    int n = 9, totalOcc;
+
+    int key = 1;
+
+    int firstIndex = firstOcc(arr, n, key);
+
+    int lastIndex = lastOcc(arr, n, key);
+    
+    cout << "First Occ: "<< firstIndex<<endl;
+    cout << "Lasr Occ: "<< lastIndex<<endl;
+
+    totalOcc = lastIndex - firstIndex + 1;
+
+    cout <<"Total number of occurence of the key is: "<<totalOcc<<endl;
+
+    
+    return 0;
+}
