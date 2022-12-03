ad86b411be1fc1b4323dc33f34cf3391ff70f6ad
vinayaksahu <vinayaksahu3@gmail.com>
2022-12-03 17:55:24 +0530

    factorial in java input by user using Loop method

--- Java/Problems/FactorialNumber.java
+++ Java/Problems/FactorialNumber.java
@@ -1,22 +1,23 @@
 import java.util.Scanner;
 
+/**
+ * FactorialNumber
+ */
 public class FactorialNumber {
-    
+
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int value = input.nextInt();
         
         int fact = 1;
 
-        //create an object of Scanner
-        Scanner input = new Scanner(System.in);
-        //take input from user
-        int num = input.nextInt();
-
-        for (int i = 1; i <= num; i++) {
-            fact *= i;
+        for (int i = 1; i <= value; i++) {
+            fact = fact  * i;
         }
         System.out.println(fact);
 
-        //close Scanner object
         input.close();
     }
 }
