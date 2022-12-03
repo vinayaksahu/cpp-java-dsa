d02b55bdb86483ed6cf129d9260f39f3b9d6fa17
vinayaksahu <vinayaksahu3@gmail.com>
2022-12-03 16:58:15 +0530

    factorial using recursion

--- C++/Problems/Basics/fatorial.cpp
+++ C++/Problems/Basics/fatorial.cpp
@@ -1,27 +1,26 @@
-#include<iostream>
-using namespace std;
+#include<iostream>    
+using namespace std;  
 
-int main() {
+int main() {   
+
+    int factorial(int);  
+      
+    int fact, value = -5;  
+
+    fact = factorial(value);   
+
+    cout<<"Factorial of a number is: "<<fact<<endl;    
+
+    return 0;    
+}    
     
-    int n = -5, fact = 1;
+int factorial(int n) {
 
-    //cin >> n;
-
-    if (n < 0) {
-
-        cout << "A -ve number does not have factorial." << endl;
-    }
-    else {
-
-        for (int i = 1; i <= n; i++) {
-            fact *= i;
-        }
-
-        cout << "Factorial: " << fact << endl;
-    }
-
-    
-    
-
-    return 0;
-}
+    if(n<0)    
+        return -1; /*Wrong value*/      
+    if(n==0)    
+        return 1;  /*Terminating condition*/    
+    else {    
+        return(n*factorial(n-1));        
+    }    
+}  
