d7eac2da2a2cc3082ba6a0fe965078e7320108c1
vinayaksahu <vinayaksahu3@gmail.com>
2022-12-03 18:33:57 +0530

    factorial in java using method with condition

--- Java/Problems/FactorialNumber.java
+++ Java/Problems/FactorialNumber.java
@@ -7,12 +7,22 @@ public class FactorialNumber {
 
     static int factorial(int n) {
 
-        int fact = 1;
+        //int fact = 1;
 
-        for (int i = 1; i <= n; i++) {
-            fact = fact  * i;
+        if (n < 0) {
+            return -1;
         }
-        return fact;
+        else if (n == 0) {
+            return 1;
+        }
+        else {
+/*
+*           for (int i = 1; i <= n; i++) {
+*              fact = fact  * i;
+*         }
+*/         return n * factorial (n -1);  
+        }
+        // return fact;
         
     }
 
