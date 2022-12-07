c0b71927f319bc81c189e18363e15da8c7292739
vinayaksahu <vinayaksahu3@gmail.com>
2022-12-07 23:11:34 +0530

    bubble sort optimized in cpp

--- C++/Problems/Sorting/bubbleSort.cpp
+++ C++/Problems/Sorting/bubbleSort.cpp
@@ -7,10 +7,15 @@ int bubbleSort(int arr[], int n) {
     int i, j;
 
     for (i = 0; i < n-1; i++) {
+        bool swapped = false;
         for (j = 0; j < n-i-1; j++) {
-            if (arr[j] > arr[j+1])
+            if (arr[j] > arr[j+1]) {
                 swap(arr[j], arr[j+1]);
-        }   
+                swapped = true;
+            }
+        }
+        if (swapped = false) 
+            break;
     }
 }
 
