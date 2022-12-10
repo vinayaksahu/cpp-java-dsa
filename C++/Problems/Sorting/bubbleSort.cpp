a11cb87912844023ce5d65ebf5da4b7c46d3f371
vinayaksahu <vinayaksahu3@gmail.com>
2022-12-10 20:07:33 +0530

    bubble sort revised

--- C++/Problems/Sorting/bubbleSort.cpp
+++ C++/Problems/Sorting/bubbleSort.cpp
@@ -6,17 +6,18 @@ int bubbleSort(int arr[], int n) {
 
     int i, j;
 
-    for (i = 0; i < n-1; i++) {
+    for (int i = 0; i < n-1; i++) {
         bool swapped = false;
-        for (j = 0; j < n-i-1; j++) {
+        for (int j = 0; j < n-i-1; i++) {
             if (arr[j] > arr[j+1]) {
                 swap(arr[j], arr[j+1]);
                 swapped = true;
             }
         }
-        if (swapped = false) 
+        if (swapped == false)
             break;
     }
+    
 }
 
 int printArray(int arr[], int n) {
