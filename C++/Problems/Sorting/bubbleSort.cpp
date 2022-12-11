534d4273d6fa988dbfc91dc145e3095612725aca
vinayaksahu <vinayaksahu3@gmail.com>
2022-12-11 22:34:14 +0530

    add array in cpp

--- C++/Problems/Sorting/bubbleSort.cpp
+++ C++/Problems/Sorting/bubbleSort.cpp
@@ -6,9 +6,9 @@ int bubbleSort(int arr[], int n) {
 
     int i, j;
 
-    for (int i = 0; i < n-1; i++) {
+    for (int i = 1; i < n-1; i++) {
         bool swapped = false;
-        for (int j = 0; j < n-i-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
             if (arr[j] > arr[j+1]) {
                 swap(arr[j], arr[j+1]);
                 swapped = true;
