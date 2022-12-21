3c7c78ef1c5d31d7b42df84e8e4135ccb291d6a2
vinayaksahu <vinayaksahu3@gmail.com>
2022-12-22 00:04:42 +0530

    array reverse in java

--- Java/3.Arrays/ArrayReverse.java
+++ Java/3.Arrays/ArrayReverse.java
@@ -3,7 +3,7 @@ public class ArrayReverse {
     public static void main(String[] args) {
         
         int[] arr = {7, 4, 8, 5, 6, 3, 2, 1, 9};
-        int n = 9;
+        int n = arr.length;
         int temp = 0;
         int s = 0, e = n-1;
 
@@ -15,6 +15,9 @@ public class ArrayReverse {
             e--;
         }
 
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
         
     }
 }
