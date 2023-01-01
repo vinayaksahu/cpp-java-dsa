bae14fd05ae6120f7c51aad05194885bbdc06bbd
vinayaksahu <vinayaksahu3@gmail.com>
2023-01-01 15:47:43 +0530

    binary search practice again

--- C++/4.Arrays & String/2.Functions and array/BinarySearch&Applications/binarySearch.cpp
+++ C++/4.Arrays & String/2.Functions and array/BinarySearch&Applications/binarySearch.cpp
@@ -5,41 +5,34 @@ int binarySearch(int a[], int n, int key) {
 
     int s = 0;
     int e = n-1;
-    int mid = s + (e - s) / 2;     /*(s - e) / 2;*/
-    
+    int mid = s+(e-s)/2;
+
     while (s <= e) {
-        
-        if (key == a[mid]) {
+        if (key ==  a[mid]) {
             return mid;
         }
-        if (key > a[mid]) {
-            s = mid + 1;
-        }
-        else /* if (key < a[mid]) */ {
+        if (key <  a[mid]) {
             e = mid - 1;
         }
-
-        mid = /* (s + e) / 2; */ s + (e - s) / 2;
+        if (key >  a[mid]) {
+            s = mid + 1;
+        }
+        mid = s+(e-s)/2;
     }
-
-    return -1;
-    
+    return -1;    
 }
 
 int main() {
     
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    int key = -8, index;
+    int key = 7, index;
 
     index = binarySearch(arr, size, key);
-    
-    if (index == -1)
-        cout << key <<" is not present in the array." << endl;
-    else
-        cout << key <<" present at index: " << index << endl;
+
+    cout << index;
 
     return 0;
 }
