ab03c5dedd906c290d04121e1aea3f9821feadbd
vinayaksahu <vinayaksahu3@gmail.com>
2023-01-02 14:25:21 +0530

    playing with pointers

--- C++/6.Pointers/playWithPointer.cpp
+++ C++/6.Pointers/playWithPointer.cpp
@@ -4,15 +4,21 @@ using namespace std;
 int main() {
     int a = 5;
 
-    /* int *ptr;
+    // int *ptr; cin >> ptr;    // bad practice
+
+    /* int *ptr = 0; 
 
     ptr = &a; */
     // or we can write like this
     int *ptr = &a;
     
-    cout << ptr << endl;
+    cout <<"Address of a: "<< ptr << endl;
+    cout <<"Value of a: "<< *ptr << endl;
+    cout <<"------------------"<< endl;
 
-    cout << *ptr << endl;
+    int *p = 0;
+    cout <<"Address of p: "<< p << endl;
+    cout <<"Value of *ptr: "<< *p << endl;
 
     return 0;
 }
