308b88082f5a42bd860bfe2be2f3e5cb54c55546
vinayaksahu <vinayaksahu3@gmail.com>
2023-01-05 15:01:01 +0530

    selection sort using swap function

--- C++/4.Arrays & String/3.Sorting/selectionSort.cpp
+++ C++/4.Arrays & String/3.Sorting/selectionSort.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
 using namespace std;
 
+void swap (int *xp, int *yp) {
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
 void selectionSort (int a[], int n) {
 
     for (int i = 0; i < n-1; i++) {
+        //find minimum element
         int min = i;
         for (int j = i+1; j < n; j++)
             if (a[min] > a[j])
                 min = j;
-        swap (a[min], a[i]);
+        //swap the found minimum element with the first element
+        swap (&a[min], &a[i]);
     }
 }
 
+//print sorted array element
 void printArray(int a[], int n) {
     for (int i = 0; i < n; i++) {
         cout << a[i] << " ";
-    } cout << endl;
-    
+    } cout << endl;    
 }
 
 int main() {
