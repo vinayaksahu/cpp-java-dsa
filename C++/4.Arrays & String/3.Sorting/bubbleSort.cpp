c9a92469f0a273d690bbfc3d75a9f8b3749a473d
vinayaksahu <vinayaksahu3@gmail.com>
2023-01-05 21:36:26 +0530

    bubble sort O(n^2)

---
+++ C++/4.Arrays & String/3.Sorting/bubbleSort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void swap (int *xp, int *yp) {
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+void bubbleSort (int a[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++)
+            if (a[j] > a[j+1])
+                swap(a[j], a[j+1]);   
+    }
+}
+
+//print sorted array element
+void printArray(int a[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    } cout << endl;    
+}
+
+int main() {
+    
+    int arr[] = {5, 4, 2, 6, 3, 9, 7, 4, 5, 2};
+
+    int size = sizeof(arr) / sizeof (arr[0]);
+
+    cout <<"Bubble Sort: "<<endl;
+    bubbleSort (arr, size);
+    printArray(arr, size);
+
+    return 0;
+}
