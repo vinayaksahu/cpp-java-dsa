1258c07a356fa9c51c51c5c51d8567771ce6ebe0
vinayaksahu <vinayaksahu3@gmail.com>
2023-01-06 20:04:23 +0530

    insertion sort

--- C++/4.Arrays & String/3.Sorting/insertionSort.cpp
+++ C++/4.Arrays & String/3.Sorting/insertionSort.cpp
@@ -3,7 +3,16 @@ using namespace std;
 
 void insertionSort (int a[], int n) {
 
-
+    for (int i = 1; i < n; i++) {
+        int j, temp = a[i];
+        for (j = i-1; j >= 0; j--) {
+            if (a[j] > temp)
+                a[j+1] = a[j];
+            else
+                break;    
+        }
+        a[j+1] = temp;   
+    }
 }
 
 //print sorted array element
