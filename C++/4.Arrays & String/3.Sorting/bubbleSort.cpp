e3ce3a52decea24a682ed8ebcca5ec831a984985
vinayaksahu <vinayaksahu3@gmail.com>
2023-01-08 12:14:52 +0530

    bubble sort done

--- C++/4.Arrays & String/3.Sorting/bubbleSort.cpp
+++ C++/4.Arrays & String/3.Sorting/bubbleSort.cpp
@@ -12,13 +12,14 @@ void bubbleSort (int a[], int n) {
     for (int i = 0; i < n-1; i++) {
         bool swapped = false;
         for (int j = 0; j < n-i-1; j++) {
-            if (a[j] > a[j+1])
+            if (a[j] > a[j+1]) {
                 swap (a[j], a[j+1]);
                 swapped = true;
-        }   
+            }
+        }
         if (swapped == false)
             break;
-    }
+    }        
 }
 
 //print sorted array element
