71507c0317fa927aa265fe471dbe65bd908d3e41
vinayaksahu <vinayaksahu3@gmail.com>
2023-01-20 02:00:39 +0530

    n stairs recursion method

---
+++ C++/7.Recursion/nStairs.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+int countDistinctWays(int nStairs) {
+    //  base case
+    if(nStairs < 0)
+        return 0;
+    if(nStairs == 0)
+        return 1;
+    
+    //RC
+    int ans = countDistinctWays(nStairs - 1) + countDistinctWays(nStairs - 2);
+    
+    return ans;
+}
