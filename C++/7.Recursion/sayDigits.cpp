546a593151fec7123bec7f080b5c39f0f89f289c
vinayaksahu <vinayaksahu3@gmail.com>
2023-01-20 02:22:19 +0530

    say digit 123 one two three

---
+++ C++/7.Recursion/sayDigits.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void sayDigit (int n, string arr[]) {
+
+    //base case
+    if (n == 0)
+        return;
+
+    //processing
+    int digit = n % 10;
+    n = n / 10;
+
+    //head recursion
+    sayDigit (n, arr);
+    cout << arr[digit] << " ";
+}
+
+int main() {
+
+    string arr[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+    int n = 123;
+
+    sayDigit (n, arr);
+
+    return 0;
+
+}
