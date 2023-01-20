83dcdf42bfc18eb5b84925a8bb9ed3595f0d54d0
vinayaksahu <vinayaksahu3@gmail.com>
2023-01-20 12:59:06 +0530

    linear search recursive method

---
+++ C++/7.Recursion/LinearSearch.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int size, int key) {
+
+    //base case
+    if (size == 0)
+        return false;
+
+    if (arr[0] == key)
+        return true;
+    
+    //Recursion call
+    else {
+        bool remainingPart = linearSearch(arr+1, size-1, key);
+        return remainingPart;
+    }
+
+}
+
+int main() {
+
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    int size = 5;
+
+    int key = 40;
+
+    bool ans = linearSearch(arr, size, key);
+
+    if (ans)
+        cout << "Present" << endl;
+    else
+        cout << "Not present" << endl;
+
+    return 0;
+
+}
