c5f929316d6a9c783956989cc7f098fe914a0bf5
vinayaksahu <vinayaksahu3@gmail.com>
2023-01-31 18:41:11 +0530

    checking syntax

---
+++ C++/Problems/checkSyntax.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    cout << 'adf' << endl ;
+    cout << 'a' << endl ;
+    cout << "b" << endl ;
+    cout << 2 << endl ; 
+
+    return 0;
+}
