71cde4d9ae36404a8644537c4c8146bb577e5faa
vinayaksahu <vinayaksahu3@gmail.com>
2023-02-01 22:19:11 +0530

    practing

--- C++/Problems/checkSyntax.cpp
+++ C++/Problems/checkSyntax.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 int main ()
 {
-    //cout << 'adf' << endl ;
-    /* cout << 'a' << endl ;
+    cout << 'adf' << endl ;
+    cout << 'a' << endl ;
     cout << "b" << endl ;
     cout << 2 << endl ;
 
     char ch = 255; 
     cout << ch << endl;
 
-     */
+    
     
     cout << (5.5/5) << end;
 
