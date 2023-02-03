577267ff83dddacd2ae93d28b96552374b9a05aa
vinayaksahu <vinayaksahu3@gmail.com>
2023-02-03 13:03:02 +0530

    explaining approach prime or not

--- DSA Master Course [Supreme Batch]/Basics of Programming - I/6.PrimeOrNot.cpp
+++ DSA Master Course [Supreme Batch]/Basics of Programming - I/6.PrimeOrNot.cpp
@@ -6,19 +6,19 @@ int main()
     int n = 5, flag = 0;
     //cin >> n;
 
-    for (int i = 2; i < n; i++)
+    for (int i = 2; i < n; i++) //  1, 2, 3, 4, 5 -> divide by 2, 3, 4
     {
-        if (n % i == 0)
+        if (n % i == 0) // if dividing by 2, 3, 4 ans is 0
         {
-            cout << "Not prime";
-            flag = 1;
-            break;
+            cout << "Not prime";    // then it is not prime
+            flag = 1; // return flag 1 as a not prime
+            break;  // break the loop
         }
     }
        
-    if (flag == 0)
+    if (flag == 0)  // if flag remains 0
     {
-        cout << "Prime";
+        cout << "Prime";    //it is prime
     }
 
     return 0;
