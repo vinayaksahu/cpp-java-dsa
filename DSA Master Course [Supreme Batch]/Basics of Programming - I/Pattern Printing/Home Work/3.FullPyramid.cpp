c9de999c63b3fd859f91249e239a22cc216a3cbc
vinayaksahu <vinayaksahu3@gmail.com>
2023-02-04 23:36:13 +0530

    practicing

--- DSA Master Course [Supreme Batch]/Basics of Programming - I/Pattern Printing/Home Work/3.FullPyramid.cpp
+++ DSA Master Course [Supreme Batch]/Basics of Programming - I/Pattern Printing/Home Work/3.FullPyramid.cpp
@@ -5,7 +5,7 @@ int main() {
     
     int n = 5;
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n-i; j++) {
+        for (int j = 0; j < n-i-1; j++) {
             cout << " "; // inverted left pyramid
         }
         
