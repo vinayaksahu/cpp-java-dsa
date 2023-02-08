f41e9b3183b6b65fa6e11b39bea909433ac9332a
vinayaksahu <vinayaksahu3@gmail.com>
2023-02-08 16:09:03 +0530

    prime or not

--- DSA Master Course [Supreme Batch]/Basics of Programming - I/Conditionals & Loops/Home Work/6.PrimeOrNot.cpp
+++ DSA Master Course [Supreme Batch]/Basics of Programming - I/Conditionals & Loops/Home Work/6.PrimeOrNot.cpp
@@ -4,19 +4,19 @@ using namespace std;
 int main()
 {
     int n = 5, flag = 0;
-    //cin >> n;
+    //cin >> n; // n is factorail or not
 
-    for (int i = 2; i < n; i++) //  1, 2, 3, 4, 5 -> divide by 2, 3, 4
+    for (int i = 2; i < n; i++) // is 5 divide by 2, 3, 4 or not
     {
         if (n % i == 0) // if dividing by 2, 3, 4 ans is 0
         {
             cout << "Not prime";    // then it is not prime
-            flag = 1; // return flag 1 as a not prime
+            flag = 1; // return flag 1 (true) as a not prime
             break;  // break the loop
         }
     }
        
-    if (flag == 0)  // if flag remains 0
+    if (flag == 0)  // if flag remains 0 (false) means 5 is not divided by 2, 3, 4
     {
         cout << "Prime";    //it is prime
     }
