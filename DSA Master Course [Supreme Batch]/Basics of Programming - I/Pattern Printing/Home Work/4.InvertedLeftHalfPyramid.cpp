bbc031d3c3a1b1880e410e6bfa60a5551bc11bf4
vinayaksahu <vinayaksahu3@gmail.com>
2023-02-09 23:24:18 +0530

    revising

--- DSA Master Course [Supreme Batch]/Basics of Programming - I/Pattern Printing/Home Work/4.InvertedLeftHalfPyramid.cpp
+++ DSA Master Course [Supreme Batch]/Basics of Programming - I/Pattern Printing/Home Work/4.InvertedLeftHalfPyramid.cpp
@@ -6,9 +6,6 @@ int main() {
     int n = 5;
 
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < i; j++) {
-            cout << " "; // space .. 
-        }
         
         for (int j = 0; j < n-i; j++) {
             cout << "*"; // .. + n-i means Left Half Pyramid
@@ -17,3 +14,12 @@ int main() {
     
     return 0;
 }
+
+
+/*
+*****
+****
+***
+**
+*
+*/
--- DSA Master Course [Supreme Batch]/Basics of Programming - I/Pattern Printing/Home Work/5.NumericFullPyramid.cpp
+++ DSA Master Course [Supreme Batch]/Basics of Programming - I/Pattern Printing/Home Work/5.NumericFullPyramid.cpp
@@ -6,9 +6,23 @@ int main() {
     int n = 5;
 
     for (int i = 0; i < n; i++) {
-        for (int j = 1; j < n-i+1; j++) { 
-            cout << j;
-        } cout << endl;
+        //spaces
+        for (int j = 0; j < n-i; j++) { 
+            cout << " ";
+        }
+ 
+        //numbers
+        for (int j = 0; j < i+1; j++) { 
+            cout << i+j+1;
+        }
+
+        //reverse counting
+        int start = 2*i;
+        for (int k = 0; k < i; k++) { 
+            cout << start;
+            start--;
+        }
+        cout << endl;
     }
 
     return 0;
