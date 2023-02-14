5116462ed6ab6f3d3c02be1d6d5fe7a989b4bd9b
vinayaksahu <vinayaksahu3@gmail.com>
2023-02-14 16:05:56 +0530

    hollow pyramid

--- DSA Master Course [Supreme Batch]/Basics of Programming - I/Pattern Printing/Home Work/6.HollowPyramid.cpp
+++ DSA Master Course [Supreme Batch]/Basics of Programming - I/Pattern Printing/Home Work/6.HollowPyramid.cpp
@@ -2,25 +2,30 @@
 using namespace std;
 
 int main() {
-    
-    int r = 5;
-    int c = 5;
 
-    for (int i = 0; i < r; i++){
-        if (i == 0 || i == 4)
-        {
-            for (int j = 0; j < c; j++) {
-            cout << "*";
+    int row = 10;
+    int col = 15;
+    
+    for (int i = 0; i < row; i+=1) {
+        if (i == 0 || i == row-1) {
+            for (int j = 0; j < col; j+=1) {
+                cout << "* ";
+            }
         }
+        else
+        {
+            cout << "* ";
+            for (int j = 0; j < col-2; j++) {
+                cout << "  ";
+            }
+            
+            cout << "* ";
         }
         
-        cout << "*";
-        for (int j = 0; j < c-2; j++) {
-            cout << " ";
-        }
-        cout << "*";
+        
         cout << endl;
     }
+    
 
     return 0;
 }
