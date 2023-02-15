1763c3767a5d088edee224773d55c663088db8d4
vinayaksahu <vinayaksahu3@gmail.com>
2023-02-15 11:30:12 +0530

    hollow full pyramid

---
+++ DSA Master Course [Supreme Batch]/Basics of Programming - I/Pattern Printing/Home Work/6.HollowFullPyramid.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    int n = 5;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n-i; j++) {
+            cout << " "; // left inverted spaces pyramid ..
+        } 
+        for (int j = 0; j < i; j++) {
+            cout << "*"; // .. + left star pyramid = right star pyramid + ...
+        }
+        for (int j = 0; j < i+1; j++) {
+            cout << "*"; // ... + left star pyramid = full star pyramid
+        }
+        
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
+/*
+
+    *     
+   ***   
+  *****  
+ ******* 
+*********
+*/
