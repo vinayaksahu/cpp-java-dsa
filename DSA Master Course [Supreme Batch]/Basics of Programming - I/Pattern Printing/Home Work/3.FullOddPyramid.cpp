839bfd2df8679178b72c99cc6915e188af8d6b2b
vinayaksahu <vinayaksahu3@gmail.com>
2023-02-15 13:42:19 +0530

    hollow full pyramid

--- DSA Master Course [Supreme Batch]/Basics of Programming - I/Pattern Printing/Home Work/3.FullOddPyramid.cpp
+++ DSA Master Course [Supreme Batch]/Basics of Programming - I/Pattern Printing/Home Work/3.FullOddPyramid.cpp
@@ -3,47 +3,39 @@ using namespace std;
 
 int main() {
     
-    int n = 15;
+    int n = 5;
 
-/*     for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         int k = 0;
         for (int j = 0; j < 2*n-1; j++) {
             if (j < n-i-1) {
                 cout << " ";
             }
             else if (k < 2*i+1) {
-                if (k == 0 || k == 2*i || i == n-1)
-                    cout << "*";
-                else
-                    cout << " ";
+                cout << "*";
                 k++;
             }
             else
                 cout << " ";
         } 
         cout << endl;
-    } */
+    }
     
+/* 
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n-i-1; j++) {
+        for (int j = 0; j < n-i; j++) {
             cout << " "; // left inverted spaces pyramid ..
         } 
         for (int j = 0; j < i+1; j++) {
-            if (j == 0 || i == n-1)
-                cout << "*"; // .. + left star pyramid = right star pyramid + ...
-            else
-                cout << " ";
+            cout << "*"; // .. + left star pyramid = right star pyramid + ...
         }
         for (int j = 0; j < i; j++) {
-            if (j == i-1 || i == n-1)
-                cout << "*"; // ... + left star pyramid = full star pyramid
-            else
-                cout << " ";
+            cout << "*"; // ... + left star pyramid = full star pyramid
         }
         
         cout << endl;
     }
-
+*/
     return 0;
 }
 
@@ -51,8 +43,8 @@ int main() {
 /*
 
     *     
-   * *   
-  *   *  
- *     * 
+   ***   
+  *****  
+ ******* 
 *********
 */
